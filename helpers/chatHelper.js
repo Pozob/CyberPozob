52cdd9a54812e017a041e5a810833928f4d368a7
Kevin Ruhnke <pozob@gmx.de>
2020-05-23 02:10:43 +0200

    Fix a Bug in the isMod Function

--- helpers/chatHelper.js
+++ helpers/chatHelper.js
@@ -1,18 +1,18 @@
 const isMod = (user, exclusive = false) => {
     if (exclusive) return user.mod;
-    return user.mod || isBroadcaster();
-}
+    return user.mod || isBroadcaster(user);
+};
 
-const isBroadcaster = (user) => {
+const isBroadcaster = user => {
     return user.badges.broadcaster;
-}
+};
 
-const isSub = (user) => {
+const isSub = user => {
     return user.subscriber;
-}
+};
 
 export default {
     isMod,
     isBroadcaster,
     isSub
-}
+};
